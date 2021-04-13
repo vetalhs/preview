486ff6fac96f36dd7edf9755f36f89deaaf02a95
Vitaliy <davinchikot2@gmail.com>
2021-04-13 09:45:29 +0300

    script.js changed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 "use strict";
 
-// const result = confirm("Сохранить изменения?");
-// console.log(result);
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?","");
+console.log(numberOfFilms);
 
-const answer = prompt("Вам есть 18?","-1");
-console.log(answer);
+
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов',''),
+      d = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
